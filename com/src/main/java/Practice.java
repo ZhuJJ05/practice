67d66f185d108392d5889276774bf65654823de3
zhujj <1825817260@qq.com>
2023-02-27 21:21:58 +0800

    第一次提交

---
+++ com/src/main/java/Practice.java
@@ -0,0 +1,12 @@
+
+public class Practice {
+    public static void main(String[] args){
+        System.out.println("111111111111111111111111");
+        System.out.print("\t");
+        System.out.println("11111\t2222");
+        System.out.println("11111\t2222");
+        System.out.println("111111\t2222");
+        System.out.print("11111111\t2222\n");
+        System.out.print("111111111\t2222");
+    }
+}
